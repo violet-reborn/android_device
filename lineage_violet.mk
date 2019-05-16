db50a9df22be6b9e8ea0c892d03f24deb734c204
Peter Cai <peter@typeblog.net>
2019-05-16 19:14:08 +0800

    build scripts: lineage-ify

--- lineage_violet.mk
+++ lineage_violet.mk
@@ -13,7 +13,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 $(call inherit-product, device/xiaomi/violet/device.mk)
 
 # Inherit some common Mokee stuff.
-$(call inherit-product, vendor/mk/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := mk_violet
